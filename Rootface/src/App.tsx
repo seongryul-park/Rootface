d00b1ea259b748a3d641a5d52df656358e627007
seongryul.park <seongryul.park@digipen.edu>
2025-01-20 17:39:22 +0900

    Initial react project setting up
    Initial react project setting up

---
+++ Rootface/src/App.tsx
@@ -0,0 +1,8 @@
+import Hello_World from './test.tsx'
+import './App.css'
+
+function App() {
+  return <div><Hello_World /></div>
+}
+
+export default App
---
+++ Rootface/src/test.tsx
@@ -0,0 +1,5 @@
+function Hello_World(){
+    return <h1>Hello world with React and Typescript!!!</h1>
+}
+
+export default Hello_World;
